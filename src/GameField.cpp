c6d2cea97119ed0440e246362a6dadf49c11dcc6
Peter Conrad <peter.conrad@uni-jena.de>
2019-11-21 14:43:32 +0100

    Add naive implementation of a game field

---
+++ src/GameField.cpp
@@ -0,0 +1,36 @@
+#include "GameField.h"
+
+GameField::GameField(const size_t rows, const size_t columns) : rows(rows), columns(columns) {
+    elements = std::vector<bool>((rows + 2) * (columns + 2), false); // padding at each side of the field
+}
+
+size_t GameField::getRows() const {
+    return rows;
+}
+
+size_t GameField::getColumns() const {
+    return columns;
+}
+
+bool GameField::getElementAt(size_t i, size_t j) {
+    return elements[indexOf(i, j)];
+}
+
+void GameField::setElementAt(size_t i, size_t j, bool value) {
+    elements[indexOf(i, j)] = value;
+}
+
+size_t GameField::indexOf(size_t i, size_t j) const {
+    return (i * rows) + j;
+}
+
+int GameField::sumOfNeighbors(size_t i, size_t j) {
+    int sum = 0;
+    for (size_t row = i - 1; row <= i + 1; i++) {
+        for (size_t col = j - 1; col <= j + 1; j++) {
+            sum += getElementAt(row, col);
+        }
+    }
+
+    return sum - getElementAt(i, j);
+}
---
+++ src/GameField.h
@@ -0,0 +1,29 @@
+#ifndef GAME_OF_LIFE_GAMEFIELD_H
+#define GAME_OF_LIFE_GAMEFIELD_H
+
+
+#include <cstdlib>
+#include <vector>
+
+class GameField {
+public:
+    GameField(size_t rows, size_t columns);
+
+    size_t getRows() const;
+    size_t getColumns() const;
+
+    bool getElementAt(size_t i, size_t j);
+    void setElementAt(size_t i, size_t j, bool value);
+
+    int sumOfNeighbors(size_t i, size_t j);
+
+private:
+    size_t indexOf(size_t i, size_t j) const;
+
+    const size_t rows;
+    const size_t columns;
+    std::vector<bool> elements;
+};
+
+
+#endif //GAME_OF_LIFE_GAMEFIELD_H
