62565a5ce3c868e683dd9b3bbf9ede0ad97fa5a9
Peter Conrad <p.conrad@protonmail.ch>
2020-01-28 16:17:05 +0100

    Allocate GameField on the stack

--- src/main.cpp
+++ src/main.cpp
@@ -8,18 +8,18 @@ int main() {
     const size_t columns = 10;
     const size_t generations = 10;
 
-    auto *frontField = new GameField(rows, columns);
-    auto *backField = new GameField(rows, columns);
-    frontField->setCentered(PRESET_GLIDER);
-    frontField->print();
+    GameField frontField(rows, columns);
+    GameField backField(rows, columns);
+    frontField.setCentered(PRESET_GLIDER);
+    frontField.print();
 
     auto start_time = omp_get_wtime();
 
     for (size_t gen = 1; gen <= generations; gen++) {
         for (size_t i = 1; i <= rows; i++) {
             for (size_t j = 1; j <= columns; j++) {
-                bool nextState = frontField->nextCellState(i, j);
-                backField->setElementAt(i, j, nextState);
+                bool nextState = frontField.nextCellState(i, j);
+                backField.setElementAt(i, j, nextState);
             }
         }
         auto tempField = frontField;
@@ -27,7 +27,7 @@ int main() {
         backField = tempField;
     }
 
-    frontField->print();
+    frontField.print();
 
     auto run_time = omp_get_wtime() - start_time;
     std::cout << "Run time for field of size " << rows << "x" << columns
