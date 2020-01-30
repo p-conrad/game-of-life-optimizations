2580398ed2807a29f1de57aee584aebbb2503b90
Peter Conrad <p.conrad@protonmail.ch>
2020-01-30 15:07:24 +0100

    Add some more interesting presets

--- src/Presets.h
+++ src/Presets.h
@@ -7,6 +7,25 @@
 const auto PRESET_GLIDER = SimpleMatrix<bool>(3, 3, {
         false, true, false,
         false, false, true,
-        true, true, true});
+        true, true, true
+});
+
+const auto PRESET_RPENTOMINO = SimpleMatrix<bool>(3, 3, {
+        false, true, true,
+        true, true, false,
+        false, true, false
+});
+
+const auto PRESET_DIEHARD = SimpleMatrix<bool>(3, 8, {
+        false, false, false, false, false, false, true, false,
+        true, true, false, false, false, false, false, false,
+        false, true, false, false, false, true, true, true
+});
+
+const auto PRESET_ACORN = SimpleMatrix<bool>(3, 7, {
+        false, true, false, false, false, false, false,
+        false, false, false, true, false, false, false,
+        true, true, false, false, true, true, true
+});
 
 #endif //GAME_OF_LIFE_PRESETS_H
