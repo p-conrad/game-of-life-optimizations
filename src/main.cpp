86bda5b1fc95d3d9572b2fc83f78065d4018ef16
Peter Conrad <p.conrad@protonmail.ch>
2020-02-03 16:08:12 +0100

    Increase field size to better fit the Eve pattern

--- src/main.cpp
+++ src/main.cpp
@@ -5,14 +5,14 @@
 #include "FieldBenchmark.h"
 
 int main(int argc, char **argv) {
-    const int rows = 300;
-    const int columns = 300;
+    const int rows = 500;
+    const int columns = 500;
     const bool output_graphics = false;
 
     GameField field(rows, columns);
     field.setCentered(PRESET_EVE);
     if (output_graphics) {
-        Fl_Double_Window win(900,900,"Game Of Life");
+        Fl_Double_Window win(1000,1000,"Game Of Life");
         GameWidget game(0, 0, win.w(), win.h(), nullptr, field);
         win.show();
         return(Fl::run());
