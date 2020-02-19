6395f408f22244166f448c4a8e61346fe4a6d2e2
Peter Conrad <p.conrad@protonmail.ch>
2020-02-19 14:41:02 +0100

    Refactor RLE parsing

--- src/RLE.h
+++ src/RLE.h
@@ -8,77 +8,79 @@
 #include "accessors.h"
 
 void readDimensions(std::string line, int &columns, int &rows) {
+    // parses the dimensions from the file header
     // remove spaces first to improve resiliency
     line.erase(std::remove_if(line.begin(), line.end(), isspace), line.end());
-    // dimensions are in the format 'x = 10, y = 10, rule= ...'
-    auto firstComma = line.find(',');
-    auto secondComma = line.substr(firstComma + 1).find(',');
-    columns = std::stoi(line.substr(2, firstComma)); // ex: x=10
-    rows = std::stoi(line.substr(firstComma + 3, secondComma)); // ex: ,y=10
+
+    // dimensions are in the format 'x = 10, y = 10[, rule= ...]'
+    auto firstEqual = line.find('=');
+    auto secondEqual = line.find('=', firstEqual + 1);
+    columns = std::stoi(line.substr(firstEqual + 1));
+    rows = std::stoi(line.substr(secondEqual + 1));
     // TODO: maybe parse the rules and check if they are compatible?
 }
 
-void readData(std::vector<uint_fast8_t> &data, std::string &line, int columns, int rows, int &y, int &x) {
-    size_t cursorStart = 0;
-    size_t cursorEnd = 0;
-    int count = 1;
+std::vector<uint_fast8_t> readData(std::ifstream &infile, int columns, int rows) {
+    // Parses the data in the file after the header has been read
+    std::vector<uint_fast8_t> result(columns * rows);
+    int x = 0;
+    int y = 0;
 
-    // remove possible line endings
-    line.erase(std::remove(line.begin(), line.end(), '\r'), line.end());
-    line.erase(std::remove(line.begin(), line.end(), '\n'), line.end());
+    std::string line;
+    while (getline(infile, line)) {
+        size_t cursor = 0;
+        int count = 1;
 
-    // read until the end of line or the terminating '!'
-    while (cursorStart < line.length() && line.at(cursorStart) != '!') {
-        // case 1: a '$' symbol indicates the next row
-        if (line.at(cursorStart) == '$') {
-            cursorStart++;
-            x = 0;
-            y = y + 1;
-            continue;
-        }
+        // remove possible line endings
+        line.erase(std::remove(line.begin(), line.end(), '\r'), line.end());
+        line.erase(std::remove(line.begin(), line.end(), '\n'), line.end());
 
-        // case 2: the cursor points at a digit - move cursorEnd until the number is fully enclosed
-        if (isdigit(line.at(cursorStart))) {
-            cursorEnd = cursorStart + 1;
-            while (isdigit(line.at(cursorEnd))) {
-                cursorEnd++;
+        while (cursor < line.length() && line.at(cursor) != '!') {
+            line = line.substr(cursor);
+            cursor = 0;
+
+            // case 1: a '$' symbol indicates the next row
+            if (line.at(cursor) == '$') {
+                cursor++;
+                x = 0;
+                y = y + 1;
+                continue;
             }
 
-            // the enclosed number is our amount of repetitions
-            count = std::stoi(line.substr(cursorStart, cursorEnd));
+            // case 2: the cursor points to a digit - we use stoi to parse the number and have
+            // cursor pointed to the first non-digit after it
+            if (isdigit(line.at(cursor))) {
+                count = std::stoi(line, &cursor);
 
-            // cursorEnd should now point at a letter indicating the state of the cell
-            // 'b' indicates a dead cell, 'o' is a living cell
-            // we take a shortcut by assuming those two are the only possible values here
-            uint_fast8_t cellState = line.at(cursorEnd) == 'b' ? 0u : 1u;
+                // 'b' indicates a dead cell, 'o' is a living cell
+                // we take a shortcut by assuming those two are the only possible values here
+                uint_fast8_t cellState = line.at(cursor) == 'b' ? 0u : 1u;
 
-            for (int n = 0; n < count; n++) {
-                set(data, columns, y, x + n, cellState);
+                for (int n = 0; n < count; n++) {
+                    set(result, columns, y, x + n, cellState);
+                }
+
+                // move x and cursorStart forward
+                x += count;
+                cursor += 1;
+                continue;
             }
 
-            // move x and cursorStart forward
-            x += count;
-            cursorStart = cursorEnd + 1;
-            continue;
+            // case 3: the cursor points at a letter - this is a litle easier than the digit case
+            if (isalpha(line.at(cursor))) {
+                uint_fast8_t cellState = line.at(cursor) == 'b' ? 0u : 1u;
+                set(result, columns, y, x, cellState);
+                x += 1;
+                cursor += 1;
+                continue;
+            }
         }
-
-        // case 3: the cursor points at a letter - this is much easier than the digit case
-        if (isalpha(line.at(cursorStart))) {
-            uint_fast8_t cellState = line.at(cursorStart) == 'b' ? 0u : 1u;
-            set(data, columns, y, x, cellState);
-            x++;
-            cursorStart++;
-            continue;
-        }
-
-        // only an ill-formatted input file should get us to this point
-        // issue a warning and advance the cursor to prevent infinite loops
-        std::cout << "Failed to read symbol '" << line.at(cursorStart) << "'\n";
-        cursorStart++;
     }
+    return result;
 }
 
 Pattern readRLE(const std::string &filename) {
+    // parses an RLE file and returns a Pattern according to its contents
     std::ifstream infile(filename);
 
     if (!infile.is_open()) {
@@ -86,25 +88,23 @@ Pattern readRLE(const std::string &filename) {
     }
 
     std::string line;
-    int columns = 0, rows = 0, y = 0, x = 0;
-    std::vector<uint_fast8_t> data;
+    int columns = 0, rows = 0;
+
+    // read file until we arrived at the header containing dimensions and rules
     while (getline(infile, line)) {
-        // first few lines may be comments, we simply ignore those
-        if (line.substr(0, 1) == "#") continue;
+        // lines starting with # are comments
+        if (line.at(0) == '#') continue;
 
-        // the first non-comment should contain the dimensions and rules separated by comma
-        if (line.substr(0, 1) == "x") {
+        // the header has the form 'x = m, y = n'
+        // once we found it and got our dimensions we are done here and leave the rest to readData
+        if (line.at(0) == 'x') {
             readDimensions(line, columns, rows);
-            data = std::vector<uint_fast8_t>(columns * rows);
-            continue;
+            break;
         }
-
-        // finally, we should have arrived at our data
-        // those might span multiple lines so we need to keep track of the row and column we are currently at
-        // it appears reasonable to assume that numbers don't appear at the end of any line
-        readData(data, line, columns, rows, y, x);
     }
 
+    auto data = readData(infile, columns, rows);
+    infile.close();
     return Pattern {rows, columns, data};
 }
 
