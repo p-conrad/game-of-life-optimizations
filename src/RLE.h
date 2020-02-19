25df59b41b6767b98dc59739c243d3936cd825d1
Peter Conrad <p.conrad@protonmail.ch>
2020-02-19 09:00:07 +0100

    Fix wrong parsing of multiline RLE files

--- src/RLE.h
+++ src/RLE.h
@@ -18,17 +18,17 @@ void readDimensions(std::string line, int &columns, int &rows) {
     // TODO: maybe parse the rules and check if they are compatible?
 }
 
-std::vector<uint_fast8_t> readData(const std::string &line, int columns, int rows) {
-    std::vector<uint_fast8_t> result(columns * rows);
-
+void readData(std::vector<uint_fast8_t> &data, std::string &line, int columns, int rows, int &y, int &x) {
     size_t cursorStart = 0;
     size_t cursorEnd = 0;
     int count = 1;
-    int x = 0;
-    int y = 0;
 
-    // read until we find the terminating "!"
-    while (line.at(cursorStart) != '!') {
+    // remove possible line endings
+    line.erase(std::remove(line.begin(), line.end(), '\r'), line.end());
+    line.erase(std::remove(line.begin(), line.end(), '\n'), line.end());
+
+    // read until the end of line or the terminating '!'
+    while (cursorStart < line.length() && line.at(cursorStart) != '!') {
         // case 1: a '$' symbol indicates the next row
         if (line.at(cursorStart) == '$') {
             cursorStart++;
@@ -53,7 +53,7 @@ std::vector<uint_fast8_t> readData(const std::string &line, int columns, int row
             uint_fast8_t cellState = line.at(cursorEnd) == 'b' ? 0u : 1u;
 
             for (int n = 0; n < count; n++) {
-                set(result, columns, y, x + n, cellState);
+                set(data, columns, y, x + n, cellState);
             }
 
             // move x and cursorStart forward
@@ -65,7 +65,7 @@ std::vector<uint_fast8_t> readData(const std::string &line, int columns, int row
         // case 3: the cursor points at a letter - this is much easier than the digit case
         if (isalpha(line.at(cursorStart))) {
             uint_fast8_t cellState = line.at(cursorStart) == 'b' ? 0u : 1u;
-            set(result, columns, y, x, cellState);
+            set(data, columns, y, x, cellState);
             x++;
             cursorStart++;
             continue;
@@ -76,8 +76,6 @@ std::vector<uint_fast8_t> readData(const std::string &line, int columns, int row
         std::cout << "Failed to read symbol '" << line.at(cursorStart) << "'\n";
         cursorStart++;
     }
-
-    return result;
 }
 
 Pattern readRLE(const std::string &filename) {
@@ -89,7 +87,7 @@ Pattern readRLE(const std::string &filename) {
     }
 
     std::string line;
-    int columns = 0, rows = 0;
+    int columns = 0, rows = 0, y = 0, x = 0;
     std::vector<uint_fast8_t> data;
     while (getline(infile, line)) {
         // first few lines may be comments, we simply ignore those
@@ -98,11 +96,14 @@ Pattern readRLE(const std::string &filename) {
         // the first non-comment should contain the dimensions and rules separated by comma
         if (line.substr(0, 1) == "x") {
             readDimensions(line, columns, rows);
+            data = std::vector<uint_fast8_t>(columns * rows);
             continue;
         }
 
         // finally, we should have arrived at our data
-        data = readData(line, columns, rows);
+        // those might span multiple lines so we need to keep track of the row and column we are currently at
+        // it appears reasonable to assume that numbers don't appear at the end of any line
+        readData(data, line, columns, rows, y, x);
     }
 
     return Pattern(rows, columns, data);
